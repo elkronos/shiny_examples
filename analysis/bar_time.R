f20f7a1385ff3e77070bf0467882028df7d82ec7
elkalytics <napoleonic_bores@proton.me>
2023-09-03 21:04:33 -0500

    Big fix
    The moving statistic line was dropping when SUM was selected. As a result I had to rework how the moving stat was calculated.

--- analysis/bar_time.R
+++ analysis/bar_time.R
@@ -7,6 +7,7 @@ library(zoo)
 library(lubridate)
 library(readr)
 
+# Define the 'bar_time' function
 bar_time <- function(data, date_col, y_col, group_by = "day", window_size = 7, bar_stat = "sum", line_stat = "mean") {
   
   if (!is.character(date_col) || !is.character(y_col) || !is.character(group_by)) {
@@ -39,7 +40,7 @@ bar_time <- function(data, date_col, y_col, group_by = "day", window_size = 7, b
   } else if (group_by == "quarter") {
     data <- data %>%
       mutate(date = ymd(date)) %>%
-      mutate(date = floor_date(date, unit = "quarter"))  # Adjusting date to the start of the quarter
+      mutate(date = floor_date(date, unit = "quarter"))
   }
   
   data_summary <- data %>%
@@ -51,14 +52,6 @@ bar_time <- function(data, date_col, y_col, group_by = "day", window_size = 7, b
   data_summary <- data_summary %>%
     arrange(date)
   
-  moving_stat <- switch(
-    line_stat,
-    mean = rollapply(data_summary$y_avg, width = window_size, FUN = mean, align = "right", fill = NA),
-    median = rollapply(data_summary$y_median, width = window_size, FUN = median, align = "right", fill = NA),
-    max = rollapply(data_summary$y_avg, width = window_size, FUN = max, align = "right", fill = NA),
-    min = rollapply(data_summary$y_sum, width = window_size, FUN = min, align = "right", fill = NA)
-  )
-  
   if (bar_stat == "sum") {
     data_summary <- data_summary %>%
       mutate(bar_value = y_sum)
@@ -69,6 +62,15 @@ bar_time <- function(data, date_col, y_col, group_by = "day", window_size = 7, b
     data_summary <- data_summary %>%
       mutate(bar_value = y_median)
   }
+  
+  data_summary <- data_summary %>%
+    mutate(moving_stat = switch(
+      line_stat,
+      mean = rollapply(bar_value, width = window_size, FUN = mean, align = "right", fill = NA),
+      median = rollapply(bar_value, width = window_size, FUN = median, align = "right", fill = NA),
+      max = rollapply(bar_value, width = window_size, FUN = max, align = "right", fill = NA),
+      min = rollapply(bar_value, width = window_size, FUN = min, align = "right", fill = NA)
+    ))
   
   date_format_string <- ifelse(group_by == "day", "%Y-%m-%d",
                                ifelse(group_by == "month", "%Y-%m",
@@ -82,7 +84,7 @@ bar_time <- function(data, date_col, y_col, group_by = "day", window_size = 7, b
   
   plot <- ggplot(data_summary, aes(x = date, y = bar_value)) +
     geom_bar(stat = "identity", fill = "steelblue", alpha = 0.8) +
-    geom_line(aes(y = moving_stat), color = "red", na.rm = TRUE, linewidth = 1, position = "identity") +
+    geom_line(aes(y = moving_stat), color = "red", na.rm = TRUE, size = 1) +
     scale_x_date(labels = date_format(date_format_string), breaks = date_breaks(date_break_interval)) +
     theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
     labs(x = "Date", y = bar_stat, title = paste("Time Series Bar Plot of", bar_stat, "with rolling", line_stat))
@@ -95,67 +97,65 @@ set.seed(123)
 example_data <- data.frame(date = seq(as.Date("2010-01-01"), as.Date("2020-12-31"), by = "day"),
                            value = rnorm(4018, 10, 2))
 
-# Function to determine the highest level of aggregation in the data
-determine_grouping <- function(data) {
-  date_values <- as.Date(data$date)
-  if (length(unique(floor_date(date_values, "year"))) > 1) return("year")
-  if (length(unique(floor_date(date_values, "quarter"))) > 1) return("quarter")
-  if (length(unique(floor_date(date_values, "month"))) > 1) return("month")
-  return("day")
+# Define the function to determine grouping
+determine_grouping <- function(date_range) {
+  date_diff <- as.numeric(difftime(date_range[2], date_range[1], units = "days"))
+  if (date_diff <= 30) {
+    return("day")
+  } else if (date_diff <= 180) {
+    return("month")
+  } else if (date_diff <= 365*2) {
+    return("quarter")
+  } else {
+    return("year")
+  }
 }
 
+# Define UI
 ui <- fluidPage(
-  navbarPage(
-    "Time Series Bar Plot with Moving Line",
-    tabPanel("Plot",
-             sidebarLayout(
-               sidebarPanel(
-                 fileInput("file", "Choose CSV File (Optional)", accept = ".csv"),
-                 selectInput("date_col", "Date Column:", choices = names(example_data), selected = if ("date" %in% names(example_data)) "date" else names(example_data)[1]),
-                 selectInput("y_col", "Value Column:", choices = names(example_data), selected = if (!"date" %in% names(example_data)) names(example_data)[2] else setdiff(names(example_data), "date")),
-                 selectInput("group_by", "Group By:", choices = c("day", "month", "quarter", "year"), selected = determine_grouping(example_data)),
-                 numericInput("window_size", "Window Size:", value = 3, min = 1),
-                 selectInput("bar_stat", "Bar Statistic:", choices = c("sum", "mean", "median"), selected = "mean"),
-                 selectInput("line_stat", "Line Statistic:", choices = c("mean", "median", "max", "min"), selected = "max")
-               ),
-               mainPanel(
-                 plotlyOutput("plot")
-               )
-             )),
-    tabPanel("Help", 
-             h4("Required Data Structure"),
-             verbatimTextOutput("help_data"))
+  titlePanel("Bar Time Analysis"),
+  sidebarLayout(
+    sidebarPanel(
+      fileInput("file", "Choose CSV File", accept = ".csv"),
+      numericInput("window_size", "Window Size:", value = 7, min = 1),
+      selectInput("bar_stat", "Bar Statistic:", choices = c("sum", "mean", "median"), selected = "sum"),
+      selectInput("line_stat", "Line Statistic:", choices = c("mean", "median", "max", "min"), selected = "mean")
+    ),
+    mainPanel(
+      plotlyOutput("plot")
+    )
   )
 )
 
+# Define server logic
+# Define server logic
 server <- function(input, output, session) {
-  data <- reactive({
-    file <- input$file
-    if (is.null(file)) {
-      return(data.frame(date = seq(as.Date("2010-01-01"), as.Date("2020-12-31"), by = "day"),
-                        value = rnorm(4018, 10, 2)))
-    }
-    read_data <- read.csv(file$datapath)
-    read_data$date <- as.Date(read_data$date)
-    return(read_data)
-  })
   
-  observe({
-    updateSelectInput(session, "date_col", choices = c("date", names(data())[names(data()) != "date"]))
-    updateSelectInput(session, "y_col", choices = names(data())[names(data()) != "date"])
-    updateSelectInput(session, "group_by", selected = determine_grouping(data()))
+  data_reactive <- reactive({
+    inFile <- input$file
+    if (is.null(inFile)) {
+      return(example_data)
+    }
+    read.csv(inFile$datapath)
   })
   
   output$plot <- renderPlotly({
-    ggplotly(bar_time(data(), input$date_col, input$y_col, input$group_by, input$window_size, input$bar_stat, input$line_stat))
+    
+    data <- req(data_reactive())
+    
+    date_col <- "date"
+    y_col <- "value"
+    
+    date_range <- range(as.Date(data[[date_col]]))
+    
+    group_by <- determine_grouping(date_range)
+    
+    plot <- bar_time(data, date_col, y_col, group_by, input$window_size, input$bar_stat, input$line_stat)
+    
+    plotly::plotly_build(plot)
   })
   
-  output$help_data <- renderPrint({
-    cat("Head of the data:\n")
-    print(head(data()))
-    cat("\nStructure of the data:\n")
-    print(str(data()))
-  })
 }
 
-shinyApp(ui, server)
+# Run the application 
+shinyApp(ui = ui, server = server)
